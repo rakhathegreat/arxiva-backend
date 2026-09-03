d9b866a046b83fc5a41c26178e9402cf969e76c3
rakhathegreat <rakhafadhillah516@gmail.com>
2026-09-03 13:46:33 +0700

    feat: increase JSON and URL-encoded body size limits to 50mb

--- app.js
+++ app.js
@@ -38,8 +38,8 @@ app.use(
 );
 
 // Middleware
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 // Swagger documentation
 setupSwagger(app, PORT);
